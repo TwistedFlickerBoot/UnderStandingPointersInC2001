0d6f43387fa509c72df7d09db26e86bceeaef394
MerlinTheSpook <chrissynrick@yahoo.com>
2025-05-07 18:51:08 -0700

    Add prog25.c

---
+++ chap2/prog25.c
@@ -0,0 +1,51 @@
+/* Program 25 */
+/* Three ways of accessing a 2-D array */
+#include <alloc.h>
+main()
+{
+    int a[3][4]={
+        1,2,3,4,
+        5,6,7,8,
+        9,0,1,6
+    };
+    clrscr();
+    display(a, 3, 4);
+    show(a, 3, 4);
+    print(a, 3, 4);
+}
+
+display(int *q, int row, int col)
+{
+    int i, j;
+
+    for(i=0; i<row; i++)
+        for(j=0; j<col; j++)
+            printf("%d ", *(q + i * col + j));
+    printf("\n");
+}
+
+show (int(*q)[4], int row, int col)
+{
+    int i, j;
+
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+            printf("%d ", *(p+j));
+        printf("\n");
+    }
+    printf("\n");
+}
+
+print(int q[][4], int row, int col)
+{
+    int i, j;
+
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+            printf("%d ", q[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
